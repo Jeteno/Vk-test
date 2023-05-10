560176d343d94664ba4039fc6e3bcc68b1f015a9
Jeteno <macsim.sytov@yandex.ru>
2023-05-10 19:06:05 +0300

    vk-test

--- script/script.js
+++ script/script.js
@@ -1,18 +1,20 @@
 function changeText(ev) {
    try {
-      if( 
-         document.getElementById("tower").value != "" 
-         && document.getElementById("floor").value != ""
-         && document.getElementById("meetingRoom").value != ""
-         && document.getElementById("date").value != ""
-         && document.getElementById("popup__text").value != ""
-         && ev.getAttribute('data-show') === "true") {
-          ev.innerText = "Отправлено";
-          ev.setAttribute('data-show', "false"); 
-      }
-      else {
-          ev.innerText = "Отправить"
-          ev.setAttribute('data-show', "true"); 
+      const isCanBeSended = document.getElementById("tower").value !== "" 
+         && document.getElementById("floor").value !== ""
+         && document.getElementById("meetingRoom").value !== ""
+         && document.getElementById("date").value !== ""
+         && document.getElementById("timeWith").value !== ""
+         && document.getElementById("timeBefor").value !== ""
+         && document.getElementById("popup__text").value !== ""
+         && ev.getAttribute('data-show') === "true";
+
+      if (isCanBeSended) {
+            ev.innerText = "Отправлено";
+            ev.setAttribute('data-show', "false"); 
+      } else {
+         ev.innerText = "Отправить";
+         ev.setAttribute('data-show', "true"); 
       }
    } catch (error) {
       console.log(error);
@@ -25,11 +27,14 @@ function clearInput() {
       document.getElementById("floor").value = "";
       document.getElementById("meetingRoom").value = "";
       document.getElementById("date").value = "";
+      document.getElementById("timeWith").value = "";
+      document.getElementById("timeBefor").value = "";
       document.getElementById("popup__text").value = "";
 
       const send = document.getElementById("sendButton");
+
       if (send.getAttribute('data-show') === "false") {
-         send.innerText = "Отправить"
+         send.innerText = "Отправить";
          send.setAttribute('data-show', "true");    
       }
    } catch (error) {
@@ -39,10 +44,10 @@ function clearInput() {
 
 function openPopup() {
    try {
-      let popup = document.getElementById("popup");
+      const popup = document.getElementById("popup");
       popup.classList.toggle("display");
    
-      let openBtn = document.getElementById("popup__open");
+      const openBtn = document.getElementById("popup__open");
       openBtn.classList.toggle("display"); 
    } catch (error) {
       console.log(error);
@@ -51,29 +56,37 @@ function openPopup() {
 
 function closePopup() {
    try {
-      let popup = document.getElementById("popup");
+      const popup = document.getElementById("popup");
       popup.classList.toggle("display");
    
-      let openBtn = document.getElementById("popup__open");
+      const openBtn = document.getElementById("popup__open");
       openBtn.classList.toggle("display");
    
-      clearInput()
+      clearInput();
    } catch (error) {
       console.log(error);
    }
 };
 
 document.forms.publish.onsubmit = function() {
-   let tower = this.tower.value;
-   let floor = this.floor.value;
-   let meetingRoom =this.meetingRoom.value;
-   let date = this.date.value;
-   let message = this.message.value;
+   const send = document.getElementById("sendButton");
 
-   const obj = {tower, floor, meetingRoom, date, message};
-   let json = JSON.stringify(obj);
+   if (send.getAttribute('data-show') === "false") {
+      const tower = this.tower.value;
+      const floor = this.floor.value;
+      const meetingRoom =this.meetingRoom.value;
+      const date = this.date.value;
+      const timeWith = this.timeWith.value;
+      const timeBefor = this.timeBefor.value;
+      const message = this.message.value;
    
-   console.log(json);
-   return false;
+      const obj = {tower, floor, meetingRoom, date, timeWith, timeBefor, message};
+      const json = JSON.stringify(obj);
+      
+      console.log(json);   
+   } else {
+      console.log("Данные уже отправлены");
+   }
 
+   return false;
  };
